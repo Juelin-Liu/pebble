c807d60085c0a56c0496a3049e1dd4180bc50d6c
juelinliu <juelinliu@umass.edu>
2024-09-15 18:57:57 +0000

    add setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
+
+pip install torch_geometric
+
+pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.4.0+cu124.html
+
+pip install --pre dgl -f https://data.dgl.ai/wheels-test/torch-2.4/cu124/repo.html
+
+pip install ninja cmake ogb torchmetrics pandas
+
+git clone -b cuda-12.4 https://github.com/Juelin-Liu/torch-quiver.git 
+
+cd torch-quiver
